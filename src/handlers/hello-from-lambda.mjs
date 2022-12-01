84c5800369f44eef201413484fd9b1773c1dfb7c
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2022-12-01 00:49:01 -0800

    updates

--- src/handlers/hello-from-lambda.mjs
+++ src/handlers/hello-from-lambda.mjs
@@ -1,6 +1,34 @@
 import aws from "aws-sdk";
 
-const db = new aws.DynamoDB({apiVersion: '2012-08-10'});
+const { TODOS_TABLE, IS_OFFLINE } = process.env;
+
+const dynamoDb = IS_OFFLINE === 'true' ?
+  new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000',
+  }) :
+  new AWS.DynamoDB.DocumentClient();
+
+const post('/emails', (req, res) => {
+  const { email, verified = false} = req.body;
+
+  const params = {
+    TableName: TODOS_TABLE,
+    Item: {
+      email,
+      verified
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log('Error creating Todo: ', error);
+      res.status(400).json({ error: 'Could not create Todo' });
+    }
+
+    res.json({ todoId, title, done });
+  });
+});
 
 export const helloFromLambdaHandler = async (event) => {
     console.log("EVENT:", event);
