356333148d51a2fd949908112d300d0b459e1a1b
Mark Stouffer <1802850+InTEGr8or@users.noreply.github.com>
2022-12-08 17:15:09 -0800

    Working after scrubbing dependencies

--- src/email-verifier.mjs
+++ src/email-verifier.mjs
@@ -1,17 +1,14 @@
+'use strict';
 import AWS from 'aws-sdk';
 import { v4 as uuidv4 } from 'uuid';
 import processResponse from './process-response.js';
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const ses = new AWS.SES();
+const SES = new AWS.SES();
 const TABLE_NAME = process.env.TABLE_NAME;
+const FROM_EMAIL = process.env.FROM_EMAIL;
 const IS_CORS = true;
-
-function isHTML(value) {
-  value = value.trim();
-  return value.startsWith('<') && value.endsWith('>') &&
-    (value.includes('<body') || value.includes('<div') || value.includes('<s') || value.includes('<h') || value.includes('<p'));
-};
+const UTF8CHARSET = 'UTF-8';
 
 function isValidEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -19,13 +16,13 @@ function isValidEmail(email) {
 }
 
 const eventToRequestData = event => {
-    var requestData = {};
+  var requestData = {};
   //Validate and parse event
   if (event && event.requestContext.http.method === 'OPTIONS') {
     return processResponse(IS_CORS);
   }
   if (event && event.requestContext.http.method === 'POST') {
-    console.log("Processing post data:", event);
+    console.log("Processing POST data:", event);
     if (!event) { return processResponse(IS_CORS, `Event object missing from call`) };
     if (!event.body) {
       console.error("MISSING EVENT BODY:", event)
@@ -41,7 +38,49 @@ const eventToRequestData = event => {
       requestData[segment.split("=")[0]] = segment.split("=")[1];
     })
   }
+  return requestData;
 }
+
+const sendVerificationAcknowledgment = async (toEmail, verificationKey) => {
+  console.log("Starting verification link process")
+  const verificationLink = `?email=${toEmail}&verificationKey=${verificationKey}`
+  const emailParams = {
+    Destination: {
+      ToAddresses: [toEmail]
+    },
+    Message: {
+      Body: {
+          Text: {
+              Charset: UTF8CHARSET,
+              Data: `Please verify your email by clicking on this link: ${verificationLink}`
+          },
+          Html: {
+              Charset: UTF8CHARSET,
+              Data: `Please verify your email by clicking this link <a href="${verificationLink}">Verification Link</a>`
+          },
+
+      },
+      Subject: {
+        Charset: UTF8CHARSET,
+        Data: "Please verify your email address"
+      }
+    },
+    Source: FROM_EMAIL,
+    ReplyToAddresses: [FROM_EMAIL]
+  };
+
+  try {
+    console.log("Sending verification link to:", emailParams);
+    const result = await SES.sendEmail(emailParams).promise();
+    console.log("Sent verification link", result);
+    return processResponse(true);
+  } catch (err) {
+    console.error(err, err.stack);
+    const errorResponse = `Error: Execution update, caused a SES error, please look at your logs.`;
+    return processResponse(true, errorResponse, 500);
+  }
+}
+
 
 export const handler = async event => {
   console.log("EVENT:", JSON.stringify(event));
@@ -49,18 +88,31 @@ export const handler = async event => {
   // 1. Upsert the db where email and event.verificationKey matches the db record
 
   var requestData = eventToRequestData(event);
+
+  // Verify and exit, if includes verificationKey
   if (requestData.verificationKey) {
     // Update email-table and set verified to true
-
+    requestData.modifiedDate = new Date().toISOString();
     dynamoDb.update({
       TableName: TABLE_NAME,
       Item: requestData
     });
-    return processResponse(IS_CORS);
+    window.location = ;
+    const redirect = {
+      statusCode: 302,
+      headers: {
+        Location: "https://handex.io/email-verified"
+      }
+    }
+    return redirect;
   }
+
+  // Validate email format
   if (!requestData.email || !isValidEmail(requestData.email)) {
     return processResponse(IS_CORS, "contact missing or malformed email", 400);
   }
+
+  // Construct new email record
   requestData.createdDate = new Date().toISOString();
   requestData.verificationKey = uuidv4();
   const params = {
@@ -69,7 +121,10 @@ export const handler = async event => {
   }
   try {
     await dynamoDb.put(params).promise()
-    return processResponse(IS_CORS);
+
+    // Send verification email
+    sendVerificationAcknowledgment(requestData.email, requestData.verificationKey);
+    return processResponse(IS_CORS, "A Verification link has been sent to the email you provided", 200);
   } catch (error) {
     let errorResponse = `Error: Execution update, caused a Dynamodb error, please look at your logs.`;
     if (error.code === 'ValidationException') {
@@ -77,31 +132,5 @@ export const handler = async event => {
     }
     console.log(error);
     return processResponse(IS_CORS, errorResponse, 500);
-  }
-  //TODO: Send verification email with verificationKey and email in link
-  const verificationLink = `?email=`
-  const emailParams = {
-    Destination: destination,
-    Message: {
-      Body: body,
-      Subject: {
-        Charset: UTF8CHARSET,
-        Data: emailData.subject
-      }
-    },
-    Source: FROM_EMAIL
-  };
-
-  if (emailData.replyToEmails && Array.isArray(emailData.replyToEmails)) {
-    emailParams.ReplyToAddresses = emailData.replyToEmails;
-  }
-
-  try {
-    await SES.sendEmail(emailParams).promise();
-    return processResponse(true);
-  } catch (err) {
-    console.error(err, err.stack);
-    const errorResponse = `Error: Execution update, caused a SES error, please look at your logs.`;
-    return processResponse(true, errorResponse, 500);
   }
 };
